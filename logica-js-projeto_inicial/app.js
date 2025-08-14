7c84a2fe1391735057a7c3c0ed0107516c0ebb00
Larissa Valli <larissavalli4@gmail.com>
2025-08-14 12:39:28 -0300

    Iniciando logica de programação com JS
    Aprendendo sobre ALERT, LET, PROMPT e IF.
    Feita atividade na prática.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,2 +1,71 @@
-alert = ("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 29;
+alert("Boas vindas ao jogo número secreto");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 a 10");
+
+ if (numeroSecreto == chute) {
+      console.log("Isso aí! Você descobriu o número secreto (5)");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------
+
+
+//Hora da prática
+// alert('Bem-vindo ao nosso site!');
+// let nome = "Lua";
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+// alert('Erro! Preencha todos os campos');
+// let mensagemDeErro = 'Erro! Preencha todos os campos';
+// alert(mensagemDeErro);
+// let nomeUser = prompt('Digite o seu nome');
+// let idadeUser = prompt('Digite a sua idade');
+// let idadeU = prompt('Digite a sua idade');
+// if (idade >= 18) {
+//     alert('Pode tirar a habilitação!'); 
+// }
+
+
+//-------------------------------------------------------------------------------------
+
+
+// alert("Estou aprendendo JavaScript");
+
+// // Pede o nome da pessoa e guarda na variável
+// let nomeUsuario = prompt("Digite seu nome");
+
+// // Pede a idade da pessoa e guarda na variável
+// let idadeUsuario = prompt("Digite sua idade");
+
+// let temPao = prompt("Você tem pão em casa? Responda com sim ou não");
+
+// if (temPao === "sim") {
+//     alert("Fazer sanduíche");
+// } else {
+//     alert("Fazer miojo");
+// }
