8935091957675bdf4c90dda961c69daca905add8
Larissa Valli <larissavalli4@gmail.com>
2025-08-14 19:12:36 -0300

    Fazendo desafios da aula

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,17 +1,80 @@
-alert("Boas vindas ao jogo número secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 a 10");
+// alert("Boas vindas ao jogo número secreto");
+// let numeroSecreto = 5;
+// console.log(numeroSecreto);
+// let chute = prompt("Escolha um número entre 1 a 10");
 
- if (numeroSecreto == chute) {
-      console.log("Isso aí! Você descobriu o número secreto (5)");
- }
+// //se chute for igual ao número secreto
+//  if (numeroSecreto == chute) {
+//       alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+//  }
+//  else{
+//      alert("Você errou!");
+//  }
 
 
+//Desafio Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+// alert("Desafio dia da semana");
+// let sabadoD = "Sábado";
+// let domingoS = "Domingo";
+// let segunda = "Segunda";
+// let terça = "Terça";
+// let quarta = "Quarta";
+// let quinta = "Quinta";
+// let sexta = "sexta";
+// let diaDaSemana = prompt("Escolha um dia da semana");
 
 
+// if(diaDaSemana == sabadoD){
+//      alert("Bom fim de semana!");
+// }
+
+// if(domingoS){
+//      alert("Bom fim de semana!");
+// } else{
+//      alert("Boa semana!");
+// }
+
+//DESAFIO CORRETO
+// diaDaSemana = prompt('Qual é o dia da semana?');
+// if (diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// } else if (diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Boa semana!');
+// }
+
+//DESAFIO CORRETO Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+// numeroPositivoOuNegativo = prompt("Digite um número positivo ou negativo");
+// if(numeroPositivoOuNegativo > 0){
+//      alert("Número positivo!");
+// } else {
+//      alert("Número negativo");
+// }
+
+//DESAFIO Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!" no console do navegador. Caso contrário, mostre "Tente novamente para ganhar.".
 
 
+// pontuacao = 105;
+// if (pontuacao >= 100) {
+//     console.log('Parabéns, você venceu!');
+// } else {
+//     console.log('Tente novamente para ganhar.');
+// }
 
+
+//DESAFIOS NÃO FEITOS
+//Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+// let saldoConta = 500; // Exemplo de saldo
+// alert(`Seu saldo é de R$${saldoConta}.`);
+
+//Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+
+// let nome = prompt('Qual o seu nome?');
+// alert(`Boas vindas ${nome}`);
 
 
 
