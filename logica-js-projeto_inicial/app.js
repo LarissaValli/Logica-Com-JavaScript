da0cd1bebe7a3068db2000af232f10a9dff59561
Larissa Valli <larissavalli4@gmail.com>
2025-08-22 11:32:19 -0300

    Desafio feito de loop while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -25,6 +25,35 @@ while (chute != numeroSecreto) {
 
 }
 
+//DESAFIO DE LOOP E WHILE
+//1.Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+// let contador = 1;
+// while (contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+//2.Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// let contagem = 10;
+// while (contagem >= 0) {
+//     console.log(contagem);
+//     contagem--;
+// }
+//3.Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+// let contagemRegressiva = prompt("Digite um número para a contagem regressiva:");
+
+// while(contagemRegressiva >= 0){
+//     console.log(contagemRegressiva);
+//     contagemRegressiva--;
+// }
+//4.Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// let contagemProgressiva = prompt("Digite um número para a contagem progressiva:");
+// let contagemDaProgressiva = 0;
+
+// while(contagemDaProgressiva <= contagemProgressiva) {
+//     console.log(contagemDaProgressiva);
+//     contagemDaProgressiva++;
+// }
+
 
 //Desafio Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
 
